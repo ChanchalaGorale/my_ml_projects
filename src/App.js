5d8c31112c1c9558d7207429e4f442070bbe719f
NektorHealth <85933978+NektorHealth@users.noreply.github.com>
2024-06-02 12:48:09 +0530

    done

--- src/App.js
+++ src/App.js
@@ -3,41 +3,10 @@ import { Avatar, Button, Typography } from "antd";
 import profile from "./assets/profile.jpeg";
 import { Divider } from "antd";
 import { Card } from "antd";
-import { Input } from "antd";
-import { useState } from "react";
-import axios from "axios";
-import CircularJSON from "circular-json";
-
-const { TextArea } = Input;
 
 const { Title } = Typography;
 
 function App() {
-  const [text, settext] = useState("");
-  const [result, setresult] = useState("");
-
-  const handleDetectlanguage = (text) => {
-    if (text) {
-      let url =
-        "https://language-detector-app-da3b8ebadb7e.herokuapp.com/predict";
-
-      const jsonString = CircularJSON.stringify(text);
-
-      axios
-        .post(url, jsonString, {
-          headers: {
-            "Content-Type": "application/json;charset=UTF-8",
-            "Access-Control-Allow-Origin": "https://cmg-ai-ml.netlify.app/",
-          },
-        })
-        .then((res) => {
-          console.log("res: ", res);
-
-          setresult(res?.data);
-        });
-    }
-  };
-
   return (
     <div>
       <div
@@ -70,32 +39,20 @@ function App() {
         }}
       >
         <Card title="Language Detector" style={{ maxWidth: "60vw" }}>
-          <TextArea
-            onChange={(e) => {
-              settext(e.target.value);
-            }}
-            value={text}
-            placeholder="Enter your text here"
-            autoSize={{ minRows: 2, maxRows: 6 }}
-          />
-
-          <Button
-            onClick={handleDetectlanguage}
-            style={{ marginTop: 20, marginBottom: 20, width: "100%" }}
-            type="primary"
-          >
-            Start Language Detector
-          </Button>
-
-          {result ? (
-            <Typography style={{ color: "#023595", textAlign: "center" }}>
-              {result}
-            </Typography>
-          ) : (
-            <Typography style={{ color: "#868686", textAlign: "center" }}>
-              Result will be shown here...
-            </Typography>
-          )}
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <Button
+              size="large"
+              type="primary"
+              onClick={() => {
+                window.open(
+                  "https://cmg-language-detector-6fa65cd6319b.herokuapp.com/",
+                  "_blank"
+                );
+              }}
+            >
+              Open App
+            </Button>
+          </div>
         </Card>
       </div>
     </div>
